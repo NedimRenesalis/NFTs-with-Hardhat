4bb635dc100e8ae1d4b0ed678544f28b3be15d34
Nedim Begić <nedim@renesalis-packaging.com>
2021-05-30 14:12:54 +0200

    Update MyCryptoLions.sol

--- contracts/MyCryptoLions.sol
+++ contracts/MyCryptoLions.sol
@@ -1,9 +1,12 @@
 pragma solidity ^0.7.3;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "Hardhat/console.sol";
 
 contract MyCryptoLions is ERC721 {
-    constructor(string memory name, string memory symbol)
-        ERC721(name, symbol)
-    {}
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        console.log("name", name);
+        console.log("symbol", symbol);
+        console.log("msg.sender", msg.sender); //msg.sender is the address that initially deploys a contract
+    }
 }
